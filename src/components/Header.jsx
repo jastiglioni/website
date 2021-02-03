6bdff05e69f7613cf8830c06ca5f2f1e798e1dfb
Jacob Castiglioni <jacobcastiglioni@Jacobs-MacBook-Pro.local>
2021-02-02 21:46:31 -0500

    cleaned up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import '../styles/General.css';
+import logo from '../static/ll.png';
 
 const padding = {
   padding: 5,
@@ -9,7 +10,8 @@ const padding = {
 const Header = () => (
 
   <div className="main">
-    <Link to="/">home</Link>
+    <Link to="/"><img src={logo} alt="logo" style={{ maxHeight: '50px', maxWidth: '50px' }} /></Link>
+    {/* <Link style={padding} to="/">home</Link> */}
     <Link style={padding} to="/about">about</Link>
     <Link style={padding} to="/blog">blog</Link>
     <Link style={padding} to="/projects">projects</Link>
